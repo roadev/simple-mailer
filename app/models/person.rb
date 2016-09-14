56da00aa219fd4b21f8b6b79a39d78cce12d2411
Juan Roa <jdroa92@gmail.com>
2016-09-14 12:25:55 -0500

    Using new model schema for programando con ruby's event

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,15 +3,19 @@ class Person
   include Mongoid::Timestamps
 
   field :name, type: String
-  field :rol, type: String, default: 'assistant'
+  #field :rol, type: String, default: 'assistant'
   field :email, type: String
   field :gender
-  field :welcome_sent, type: Boolean, default: false
+  field :event, type: Integer
+  field :age, type: Integer
+  field :company, type: String
+  #field :welcome_sent, type: Boolean, default: false
 
-  validates_presence_of :name, :rol, :email
+  validates_presence_of :name, :email, :event
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  #validates_format_of :event, with: /^(\d)+$/
 
-  scope :by_rol, lambda{|rol| where(rol: rol)}
+  scope :event_rol, lambda{|event| where(event: event)}
 
   def first_name
     name.present? ? name.split(' ')[0].capitalize : ''
--- app/services/import_people.rb
+++ app/services/import_people.rb
@@ -1,25 +1,5 @@
 require 'csv'
 
-#
-# Class ImportPeople provides a logic for importing people from a csv file
-#
-# @author Johan Tique <johan.tique@analisistem.com>
-#
-class ImportPeople
-  #
-  # Just a call method
-  #
-  # @param [ File ] import_people_form
-  #
-  # @return [ Boolean ]
-  #
-  def call(import_people_form)
-    CSV.foreach(import_people_form.file.tempfile, headers: true) do |row|
-      Person.find_or_create_by name: row['Name'].to_s.downcase, email: row['Email'].downcase, gender: row['Gender'], rol: row['Role']
-    end
-    true
-  rescue => e
-    Rails.logger.error e.message
-    false
+  CSV.foreach('./evento.csv', headers: true) do |row|
+    Person.find_or_create_by name: row['Name'].to_s.split.map(&:capitalize).join(' '), email: row['Email'].to_s.downcase, gender: row['Gender'].to_s.downcase, event: row['Event'].to_i, company: row['Company'].to_s.split.map(&:capitalize).join(' ')
   end
-end
