e0ec4f1b304c4000538443a7bdc2b1087f665e10
Juan Roa <jdroa92@gmail.com>
2017-02-12 01:04:26 -0500

    Adding new person fields

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,18 @@ class Person
   field :name, type: String
   field :role, type: String, default: 'assistant'
   field :email, type: String
-  field :gender
+  field :gender, type: String
+  field :register_date, type: Date
+  field :birth_date, type: Date
+  field :phone, type: String
+  field :level, type: String
+  field :motivation, type: String
+  field :expectation, type: String
+  field :heard_ror, type: String
+  field :laptop, type: Boolean
+  field :so, type: String
+  field :attended, type: String
+  field :how_did_you_find_it, type: String
 
   validates_presence_of :name, :role, :email
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
